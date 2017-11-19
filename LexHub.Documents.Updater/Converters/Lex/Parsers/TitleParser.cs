c95ccf407719f045c2828a3b685e3d9847e594c8
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-11-20 00:12:52 +0100

    title parser

---
+++ LexHub.Documents.Updater/Converters/Lex/Parsers/TitleParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using LexHub.Documents.Models;
+
+namespace LexHub.Documents.Updater.Converters.Lex.Parsers
+{
+    class TitleParser : BaseUnitStringParser
+    {
+        public TitleParser(IParserFactory parserFactory) : base(parserFactory)
+        {
+            PossibleSubUnits = new HashSet<UnitType>
+            {
+                UnitType.Section
+            };
+        }
+
+        protected override async Task<ActUnit> ParseMetadata(StringReader source)
+        {
+            var nameLine = await source.ReadLineAsync();
+            var titleLine = await source.ReadLineAsync();
+
+            var result = nameLine.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+            return new ActUnit
+            {
+                Type = UnitType.Title,
+                Title = titleLine,
+                Number = result[1]
+            };
+        }
+
+        protected override HashSet<UnitType> PossibleSubUnits { get; }
+    }
+}
